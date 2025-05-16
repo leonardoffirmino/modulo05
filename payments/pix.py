73989d58bd157443aa25bf0a7cf896f2e7c0388c
Leonardo <firmino750@gmail.com>
2025-05-15 21:18:02 -0300

    Create first case for tests in application with python

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix():
   def __init__(self):
     pass
 
-  def create_payment(self):
+  def create_payment(self,base_dir=""):
     #Criar pagamento na instituição financeira para gerar esse token para dar a notificação do pagamento.
     bank_payment_id = str(uuid.uuid4())
 
@@ -13,7 +13,7 @@ class Pix():
     hash_payment = f'hash_payment_{bank_payment_id}'
 
     img = qrcode.make(hash_payment)
-    img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+    img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
     return{
       "bank_payment_id":bank_payment_id,
